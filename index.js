3d7b1043aca879d34ab0fb572a14ff12938ae44c
Samanuay <200303105038@paruluniversity.ac.in>
2022-07-29 10:49:22 +0530

    Json Api Deploy

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const app = express();
+const http = require("http");
 const api = require("./api.json");
 const port = 3000;
 
+const server = http.createServer(app);
+
 app.get("/", (req,res)=>{
-    res.send("Simple Json Api");
+    res.send("Express Server home page..");
 });
 
 app.get("/api", (req,res)=>{
-    res.send(api);
+    res.send(JSON.stringify(api));
 });
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server running on http://localhost:${port}`);
 });
